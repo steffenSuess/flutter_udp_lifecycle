cd9a01e89adf9f332983fe973745a1e0b5fe9686
Steffen Suess <steffen.suess@outlook.de>
2020-12-10 11:53:06 +0100

    -implemented Firebase_Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_udp_lifecycle/provider/lifecycle_provider.dart';
@@ -11,6 +14,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -22,7 +27,36 @@ class MyApp extends StatelessWidget {
           create: (_) => UdpProvider(),
         ),
       ],
-      child: AppLifecycleReactor(),
+      child: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              return MaterialApp(
+                home: Scaffold(
+                  body: Center(
+                    child: Text(snapshot.error.toString()),
+                  ),
+                ),
+              );
+            }
+
+            // Once complete, show your application
+            if (snapshot.connectionState == ConnectionState.done) {
+              return AppLifecycleReactor();
+            }
+
+            // Otherwise, show something whilst waiting for initialization to complete
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Platform.isIOS
+                      ? CupertinoActivityIndicator()
+                      : CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
